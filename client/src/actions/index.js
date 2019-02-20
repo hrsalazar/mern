e10ccd09c827c61436b88760bc0d52b866762d4e
Hector Salazar <hector.sa@gmail.com>
2019-02-20 21:13:07 +1100

    First action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,6 +3,11 @@ import axios from 'axios';
 import { FETCH_USER } from './types';
 
 const fetchUser = () => {
-    axios.get('/api/current_user', FETCH_USER);
-
+    //Dispatch is a function from store
+    return function(dispatch){
+        //ReduxThunk gives the ability to dispatch and action when ever we need it not automatically
+        axios
+            .get('/api/current_user')
+            .then(res => dispatch({ type: FETCH_USER, payload: res }));
+    }
 };
--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,14 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
-
-
+/*ReduxThunk is a lib that allow us to send an action to the store.dispatch 
+reaching all the reducers */
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App.js';
 import reducers from './reducers';
 
-const store = createStore( reducers, {}, applyMiddleware());
+const store = createStore( reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDom.render(
     <Provider store={store}><App /></Provider>,
